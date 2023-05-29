f01e78b55eb52d1d76ec1f8e9b5914e53da9604f
Vitor Aderaldo <vitor.aldero@harmonize.health>
2023-05-29 16:33:48 -0300

    ci: deploy version 3.0.0

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker context use default
-docker buildx build --push --tag vitor96k/arithmetic-backend:2.0.0 -o type=image --platform=linux/arm64,linux/amd64 . -f Dockerfile
+docker buildx build --push --tag vitor96k/arithmetic-backend:3.0.0 -o type=image --platform=linux/arm64,linux/amd64 . -f Dockerfile
 docker context use arithmetic-staging
 ENVIRONMENT=staging docker compose up
