83ebc25697ebfa8ba90ca63b1d552bbd38da8a16
Vitor Aderaldo <vitor.aldero@harmonize.health>
2023-06-03 01:31:34 -0300

    refactor: add presenter to display data in xml

--- src/infra/record/routes/records.controller.ts
+++ src/infra/record/routes/records.controller.ts
@@ -4,15 +4,17 @@ import {
   Get,
   Param,
   Query,
+  Res,
   UseGuards,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthGuard } from '../../guards/auth.guard';
 import { SearchRecordsUseCase } from '../../../usecase/record/serch-records.usecase';
 import { SearchRecordsRequest } from './requests/search-records.request';
-import { SearchRecordsOutputDto } from '../../../usecase/record/dto/search-records.dto';
 import { IdentityProviderId } from '../../guards/identity-provider-id.decorator';
 import { DeleteRecordUseCase } from '../../../usecase/record/delete-record.usecase';
 import { DeleteRecordOutputDto } from '../../../usecase/record/dto/delete-record.dto';
+import { RecordsPresenter } from './presenters/record.presenter';
 
 @UseGuards(AuthGuard)
 @Controller('/records')
@@ -23,11 +25,12 @@ export class RecordsController {
   ) {}
 
   @Get('/')
-  public searchRecords(
+  public async searchRecords(
     @Query() params: SearchRecordsRequest,
     @IdentityProviderId() identityProviderId: string,
-  ): Promise<SearchRecordsOutputDto> {
-    return this.searchRecordsUseCase.execute({
+    @Res() response: Response,
+  ): Promise<void> {
+    const result = await this.searchRecordsUseCase.execute({
       filter: {
         identityProviderId,
         operationId: params.operationId,
@@ -42,6 +45,11 @@ export class RecordsController {
         page: params.page,
         pageSize: params.pageSize,
       },
+    });
+
+    response.format({
+      json: () => response.json(result),
+      xml: () => response.send(RecordsPresenter.toXml(result)),
     });
   }
 
