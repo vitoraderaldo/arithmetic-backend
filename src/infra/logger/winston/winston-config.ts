83884ca9a1e904bcfeb67fc609281ac8bef68cd3
Vitor Aderaldo <vitor.aldero@harmonize.health>
2023-06-18 16:19:58 -0300

    feat: use volume to store the logs

--- src/infra/logger/winston/winston-config.ts
+++ src/infra/logger/winston/winston-config.ts
@@ -28,13 +28,9 @@ export const winstonLogger = createLogger({
   ),
   transports: [
     new transports.File({ filename: '.logs/arithmetic-calculator.log' }),
-  ],
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  winstonLogger.add(
     new transports.Console({
       format: format.combine(format.colorize(), format.simple()),
     }),
-  );
-}
+  ],
+});
+
